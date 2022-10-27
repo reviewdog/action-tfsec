6ebc33f98d206fbe9cc571efca19d1743b7b2aa5
Karan Kohli <karan.kohli@contentful.com>
2022-10-27 02:45:49 +0200

    try

--- script.sh
+++ script.sh
@@ -10,53 +10,21 @@ set -Eeuo pipefail
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKING_DIRECTORY}" || exit
 
-echo '::group::Preparing ...'
-  unameOS="$(uname -s)"
-  case "${unameOS}" in
-    Linux*)     os=linux;;
-    Darwin*)    os=darwin;;
-    CYGWIN*)    os=windows;;
-    MINGW*)     os=windows;;
-    MSYS_NT*)   os=windows;;
-    *)          echo "Unknown system: ${unameOS}" && exit 1
-  esac
-
-  unameArch="$(uname -m)"
-  case "${unameArch}" in
-    x86*)      arch=x64;;
-    *)         echo "Unsupported architecture: ${unameArch}. Only x64 is supported by kics" && exit 1
-    esac
-
+# echo '::group::Preparing ...'
   TEMP_PATH="$(mktemp -d)"
-  echo "Detected ${os} running on ${arch}, will install tools in ${TEMP_PATH}"
   REVIEWDOG_PATH="${TEMP_PATH}/reviewdog"
-  KICS_PATH="${TEMP_PATH}/kics"
-echo '::endgroup::'
+# echo '::endgroup::'
 
 echo "::group::🐶 Installing reviewdog (${REVIEWDOG_VERSION}) ... https://github.com/reviewdog/reviewdog"
   curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${REVIEWDOG_PATH}" "${REVIEWDOG_VERSION}" 2>&1
 echo '::endgroup::'
 
-echo "::group:: Installing kics (${INPUT_KICS_VERSION}) ... https://github.com/Checkmarx/kics"
-  test ! -d "${KICS_PATH}" && install -d "${KICS_PATH}"
-
-  if [[ "${INPUT_KICS_VERSION}" = "latest" ]]; then
-    kics_version=$(curl --silent -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${INPUT_GITHUB_TOKEN}" https://api.github.com/repos/Checkmarx/kics/releases/latest | jq -r .tag_name)
-  else
-    kics_version=${INPUT_KICS_VERSION}
-  fi
-  binary="kics"
-  url="https://github.com/Checkmarx/kics/releases/download/v${kics_version}/kics_${kics_version}_${os}_${arch}.tar.gz"
-
-  curl --silent --show-error --fail \
-    --location "${url}" \
-    --output kics_${kics_version}_${os}_${arch}.tar.gz
-  tar -xvzf kics_${kics_version}_${os}_${arch}.tar.gz
-  install kics "${KICS_PATH}"
-echo '::endgroup::'
+# echo "::group:: Installing kics (${INPUT_KICS_VERSION}) ... https://github.com/Checkmarx/kics"
+curl -sfL 'https://raw.githubusercontent.com/Checkmarx/kics/master/install.sh' | bash
+# echo '::endgroup::'
 
 echo "::group:: Print kics details ..."
-  "${KICS_PATH}/kics" version
+  "kics" version
 echo '::endgroup::'
 
 echo '::group:: Running kics with reviewdog 🐶 ...'
@@ -66,8 +34,7 @@ echo '::group:: Running kics with reviewdog 🐶 ...'
   set +Eeuo pipefail
 
   # shellcheck disable=SC2086
-  export KICS_QUERIES_PATH=./assets
-  "${KICS_PATH}/kics" scan --path ${INPUT_KICS_SCAN_PATH} --output-name kics --output-path . --report-formats json ${INPUT_KICS_FLAGS:-} 
+  kics_exit=$("kics" scan --path ${INPUT_KICS_SCAN_PATH} --output-name kics --output-path . --report-formats json ${INPUT_KICS_FLAGS:-})
   jq -r -f "${GITHUB_ACTION_PATH}/to-rdjson.jq" kics.json \
     |  "${REVIEWDOG_PATH}/reviewdog" -f=rdjson \
         -name="kics" \
@@ -77,7 +44,7 @@ echo '::group:: Running kics with reviewdog 🐶 ...'
         -filter-mode="${INPUT_FILTER_MODE}" \
         ${INPUT_FLAGS}
 
-  kics_return="${PIPESTATUS[0]}" reviewdog_return="${PIPESTATUS[2]}" exit_code=$?
+  kics_return="${kics_exit}" reviewdog_return="${PIPESTATUS[1]}" exit_code=$?
   echo "::set-output name=kics-return-code::${kics_return}"
   echo "::set-output name=reviewdog-return-code::${reviewdog_return}"
 echo '::endgroup::'
